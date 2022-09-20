94a7d63637ac20a033983ec0bde89246808a6398
pyonw <91231803+pyonw@users.noreply.github.com>
2022-09-19 22:55:32 -0700

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Woo Pyon on 9/19/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+        
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
